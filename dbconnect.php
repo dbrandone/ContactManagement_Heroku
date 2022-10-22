40539cfcd9303c52e365f53702f4f2ace97c9946
dbrandone <115263503+dbrandone@users.noreply.github.com>
2022-10-21 20:14:48 -0500

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,14 @@
 <?php
-//$serverName = "eu-az-sql-serv1.database.windows.net"; // update me
-//$connectionOptions = array(
-                    //"Uid" => "upjuoq7bx5i2qav", // update me
-                    //"PWD" => "9y7Xlbah#TeRWYFD3hhPK3hLI", // update me
-                    //"Database" => "d4inlpy2hl419fl" // update me
-//);
+$serverName = "eu-az-sql-serv1.database.windows.net"; // update me
+$connectionOptions = array(
+                    "Uid" => "upjuoq7bx5i2qav", // update me
+                    "PWD" => "9y7Xlbah#TeRWYFD3hhPK3hLI", // update me
+                    "Database" => "d4inlpy2hl419fl" // update me
+);
     //Establishes the connection
 //$connect = SQLSRV_CONNECT($serverName, $connectionOptions);
     
-    $connect = new mysqli('127.0.0.1:53542', 'azure', '6#vWHD_$', 'contactmanagement');
+    $connect = new mysqli('$ServerName', '$connectionOptions);
 
         if(!$connect){
             die("Can't Connect to SQL Server");
