93531da424f23f470c688459124b2223893ca30f
dbrandone <115263503+dbrandone@users.noreply.github.com>
2022-10-21 21:05:03 -0500

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -7,7 +7,7 @@ $database = "contactmanagement"; // update me
     //Establishes the connection
 //$connect = SQLSRV_CONNECT($serverName, $connectionOptions);
     
-    $connect = new mysqli($serverName, $username, $password, $database);
+    $connect = new mysqli_connect($serverName, $username, $password, $database);
 
         if(!$connect){
             die("No Connection to SQL Server");
