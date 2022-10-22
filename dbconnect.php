260310f3918c0bfacd52769c51614fa935d034af
dbrandone <115263503+dbrandone@users.noreply.github.com>
2022-10-21 20:18:54 -0500

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -11,7 +11,7 @@ $connectionOptions = array(
     $connect = new mysqli($ServerName, $connectionOptions);
 
         if(!$connect){
-            die("No Connection to SQL Server": $connect);
+            die("No Connection to SQL Server");
         }
     
 ?>
