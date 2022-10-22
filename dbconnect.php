cb4eabd18fcee838d1b94779b6c1627c5db0b81b
dbrandone <115263503+dbrandone@users.noreply.github.com>
2022-10-21 20:24:50 -0500

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,13 @@
 <?php
 $serverName = "eu-az-sql-serv1.database.windows.net"; // update me
-$connectionOptions = array(
-                    "Uid" => "upjuoq7bx5i2qav", // update me
-                    "PWD" => "9y7Xlbah#TeRWYFD3hhPK3hLI", // update me
-                    "Database" => "d4inlpy2hl419fl" // update me
-);
+$username = "upjuoq7bx5i2qav"; // update me
+$password = "9y7Xlbah#TeRWYFD3hhPK3hLI"; // update me
+$database = "d4inlpy2hl419fl"; // update me
+
     //Establishes the connection
 //$connect = SQLSRV_CONNECT($serverName, $connectionOptions);
     
-    $connect = new mysqli($serverName, $connectionOptions);
+    $connect = new mysqli($serverName, $username, $password, $database);
 
         if(!$connect){
             die("No Connection to SQL Server");
