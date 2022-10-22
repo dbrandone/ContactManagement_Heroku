850c043e1e64ba41b3f476fa594cc0f5115f7fef
dbrandone <115263503+dbrandone@users.noreply.github.com>
2022-10-21 19:55:37 -0500

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,8 +2,8 @@
 $serverName = "eu-az-sql-serv1.database.windows.net"; // update me
 $connectionOptions = array(
                     "Uid" => "upjuoq7bx5i2qav", // update me
-                    "PWD" => "9y7Xlbah#TeRWYFD3hhPK3hLI" // update me
-                    "Database" => "d4inlpy2hl419fl", // update me
+                    "PWD" => "9y7Xlbah#TeRWYFD3hhPK3hLI", // update me
+                    "Database" => "d4inlpy2hl419fl" // update me
 );
     //Establishes the connection
 $connect = sqlsrv_connect($serverName, $connectionOptions);
